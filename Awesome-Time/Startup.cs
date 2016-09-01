ee009f71d5939719f69746c162d00ea3bc4c90f5
dobripanchev@gmail.com <almalexia19>
2016-09-01 15:35:28 +0300

    Initial Commit
    Basic ASP.NET MVC project template

---
+++ Awesome-Time/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Awesome_Time.Startup))]
+namespace Awesome_Time
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
